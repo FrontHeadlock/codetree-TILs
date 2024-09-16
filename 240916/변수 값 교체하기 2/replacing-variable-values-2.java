3aedb3d05fc09757eab9ea0c97e351ba6b135b49
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-09-16 20:54:09 +0900

    Title: 변수 값 교체하기 2, Time: 90ms, Memory: 8MB, Status: Passed - CodeTree

--- 240916/변수 값 교체하기 2/replacing-variable-values-2.java
+++ 240916/변수 값 교체하기 2/replacing-variable-values-2.java
@@ -2,9 +2,8 @@ public class Main {
     public static void main(String[] args) {
         int a = 5;
         int b = 3;
-        int temp = a;
-        int a = b;
-        b = temp;
-        System.out.printf("%d %d",a,b);
+        int temp = b;
+        a = temp;
+        System.out.printf("%d\n%d",a,b);
     }
 }
