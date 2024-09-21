77b90ffe157e26fc818c204690ede7ae2bad4ed8
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-09-21 15:27:34 +0900

    Title: 최대 2번의 연산, Time: 129ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240921/최대 2번의 연산/up-to-2-calculations.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        if(a%2==0){
+            int k = a/2;
+            if(k%2==1){
+                int k1 = (k+1)/2;
+                System.out.print(k1);
+            }
+        }
+        else{
+            int q = (a+1)/2;
+            System.out.print(q);
+        }
+
+    }
+}
