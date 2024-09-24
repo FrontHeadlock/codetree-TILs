3523e525c5cb563972dd25392186499c68e8816b
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-09-24 23:39:04 +0900

    Title: 조건에 부합하는 수, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 240924/조건에 부합하는 수/number-that-meets-the-condition.java
+++ 240924/조건에 부합하는 수/number-that-meets-the-condition.java
@@ -8,7 +8,7 @@ public class Main {
         //int cnt = 0;
 
         for(int i=1;i<=a;i++){
-            if(i%2==0 && a%4!=0){
+            if(i%2==0 && i%4!=0){
                 continue;
             }
             else if((i/8)%2==0){
