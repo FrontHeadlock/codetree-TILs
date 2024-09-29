1018004caf29e7c50bffb3923006cc847d64ae4d
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-09-29 15:18:35 +0900

    Title: 별표 출력하기 5, Time: 117ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240929/별표 출력하기 5/print-star-5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        for (int i = n; i >= 2; i--) {
+            for (int k = 1; k <= 4; k++) {
+                if (k <= i) {
+                    for (int j = 1; j <= i; j++) {
+                        System.out.print("*");
+                    }
+                    if (k < i) {
+                        System.out.print(" ");
+                    }
+                }
+            }
+            System.out.println();
+        }
+        System.out.println("*");
+    }
+}
