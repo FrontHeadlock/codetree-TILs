862997bcf110696a823ba83fec534521449ccda7
Dobby19 <150355097+FrontHeadlock@users.noreply.github.com>
2024-10-03 17:42:33 +0900

    Title: 별표 출력하기, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 241003/별표 출력하기/print-star.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = n;
+        for (int i = 1; i <= m; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        for (int i = m - 1; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
+}
